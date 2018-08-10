1a6918ddad5b239254fc8cca507ffd8022b2a991
poojitha18 <mnpoojitha@gmail.com>
2018-08-10 12:21:24 +0530

    Question added

--- src/com/company/Driver.java
+++ src/com/company/Driver.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class Driver {
 
     public static void main(String[] args) throws Exception {
-        File file = new File("C:\\Users\\nmalla\\Documents\\FolioNumbers\\src\\com\\company\\input.txt");
+        File file = new File("C:\\Users\\nmalla\\Documents\\input.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
         String line;
 
@@ -22,7 +22,6 @@ public class Driver {
                 tempSet.add(line);
             }
         }
-
-        System.out.println(testCases.size());
+        
     }
 }
